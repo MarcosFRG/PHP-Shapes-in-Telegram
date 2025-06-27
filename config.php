15d4f0d712fb8ba8e257f003e5ab02e057185dd6
MarcosFRG <marcosfrgames2@gmail.com>
2025-06-26 20:57:48 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Tu contraseña secreta para añadir comandos.
 $SITE_PASS = 'TU_CONTRASEÑA';
 // Tamaño máximo de documentos en KiB (ten en cuenta que esto llena el contexto del modelo)
-$MAX_DOCSIZE = 5;
+$MAX_DOCSIZE = 50;
 // Probabilidad de que el bot añada una reacción a mensajes dirijidos a este (actual: 1/30)
 $REACT_PROB1 = 30;
 // Probabilidad de que añada una reacción a mensajes no dirijidos a este (actual: 1/80)
