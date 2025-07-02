dcd8a17b365cb1b9d692c15a3b1bde2e5b24b5d9
MarcosFRG <marcosfrgames2@gmail.com>
2025-07-02 12:52:17 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Tu contraseña secreta para añadir comandos.
 $SITE_PASS = 'TU_CONTRASEÑA';
-// Tamaño máximo de documentos en KiB (ten en cuenta que esto llena el contexto del modelo)
+// Tamaño máximo de documentos en KiB (ten en cuenta que esto llena el contexto del modelo, aparte de que hay un límite de caracteres puesto por Shapes, Inc. API)
 $MAX_DOCSIZE = 15;
 // Tamaño máximo de PDFs a procesar en MiB (este no llenaría tanto el contexto del modelo, ya que solo se extrae el texto)
 $MAX_PDFSIZE = 100;
