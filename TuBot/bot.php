865b3e625741cb3eb4f03ba180dcd2dd3eb98b3e
MarcosFRG <marcosfrgames2@gmail.com>
2025-07-30 13:08:14 -0400

    Add files via upload
    v0.91

--- TuBot/bot.php
+++ TuBot/bot.php
@@ -1,22 +1,28 @@
 <?php
-// Tu webhook de Telegram debe dirijirse a este archivo
+// Tu webhook de Telegram debe dirijirse a este archivo.
 $TELEGRAM_TOKEN = '123:BOTLOL';
 $SHAPES_API_KEY = 'TU_KEY_DE_SHAPESINC';
 $SHAPE_USERNAME = 'tu-shape'; // Ej: lilly-suzuki
 $SHAPE_NAME = 'Nombre de tu shape';
 $BOT_USERNAME = 'Tu_bot'; // Ej: "Lilly_Suzuki_shape_bot" (SIN EL @)
 
-// El shape responderá siempre cuando exista, mínimo,
-// una de estas palabras (opcional)
-$Favorite_words = ["LOL", "KKDBB", "oye", "hey"];
+// Las siguientes 4 variables son opcionales.
+$START_MSG = "Hola.";
+$ERROR_MSG = "? 💀";
+$ACTIVATE_MSG = "Activad@ ✅";
+$DEACTIVATE_MSG = "Desactivad@ ❌";
+
+// Tu bot responderá siempre cuando exista, mínimo, una de estas palabras (variable opcional)
+$Favorite_words = ["Palabra 1", "Palabra 2", "Palabra 3"];
 
 // Formato "palabra" => [reacciones...] (opcional)
 $Reactions = [
-  "KK" => ["👀", "🔥", "💩"],
-  "Good" => ["👍", "💯", "🔥", "🆒"],
-  "Kionda" => ["🖐︄1�7", "👻", "🗿", "🆒"]
-  ];
+"KK" => ["👀", "🔥", "💩"],
+"Good" => ["👍", "💯", "🔥", "🆒"],
+"Kionda" => ["👌", "👻", "🗿", "🆒"]
+];
 
+// Activar comandos (opcional)
 $SHAPE_COMMAND_DICE = true;
 $SHAPE_COMMAND_8BALL = true;
 
